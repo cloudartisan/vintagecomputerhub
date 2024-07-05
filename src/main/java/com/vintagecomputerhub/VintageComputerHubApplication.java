08a5d46e2cd5c890ac9d5d1f4a5a216d97ef1c66
David Taylor <>
2024-07-05 16:31:00 +1000

    Initial commit

---
+++ src/main/java/com/vintagecomputerhub/VintageComputerHubApplication.java
@@ -0,0 +1,13 @@
+package com.vintagecomputerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VintageComputerHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VintageComputerHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vintagecomputerhub/VintageComputerHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vintagecomputerhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VintageComputerHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
